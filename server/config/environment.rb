3c3d48044b8bfab28213a46571f514d98776cd27
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-08-10 16:03:48 -0700

    Add RubyCAS-client configuration

--- server/config/environment.rb
+++ server/config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Server::Application.initialize!
+
+require 'casclient/frameworks/rails/filter'
+
+CASClient::Frameworks::Rails::Filter.configure(
+  :cas_base_url => "https://slim/rubycas"
+)
