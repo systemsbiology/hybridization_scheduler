2add685e29f3f2bac9ac1fb5377e75d2f1f3978e
Bruz Marzolf <bmarzolf@systemsbiology.org>
2010-09-01 14:34:24 -0700

    Have server update action use the session user login and output json

--- server/app/controllers/reservations_controller.rb
+++ server/app/controllers/reservations_controller.rb
@@ -12,4 +12,15 @@ class ReservationsController < InheritedResources::Base
     @reservation = Reservation.new( params["reservation"].merge({:user_login => session[:cas_user]}) )
     create!
   end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+
+    # merge in the user identity from the session
+    @reservation.update_attributes( params["reservation"].merge({:user_login => session[:cas_user]}) )
+
+    super do |format|
+      format.json { render :json => @reservation }
+    end
+  end
 end
